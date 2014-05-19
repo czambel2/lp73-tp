e25e7934ae47f3e5e1d926d51d8f9e64d0929dbe
czambel2 <zmbl.soft@gmail.com>
2014-05-19 09:49:41 +0200

    Removes redudant comments in headers

--- objet/lib/IntegerArray.h
+++ objet/lib/IntegerArray.h
@@ -1,15 +1,3 @@
-/**
- * Écrire une classe qui définit un conteneur d'entiers, qui contient les
- *  méthodes suivantes :
- * - void pushback(int v) pour ajouter un entier à la fin de la structure,
- * - int at(int i) pour accéder au i-ième élément,
- * - void clear() pour vider la structure,
- * - void popback() pour supprimer le dernier élément de la structure,
- * - int front() pour accéder au premier élément,
- * - int back() pour accéder au dernier élément,
- * - void set(int i, int v) pour modifier le i-ième élément.
- */
-
 #include "IntegerContainer.h"
 
 class IntegerArray: public IntegerContainer {
